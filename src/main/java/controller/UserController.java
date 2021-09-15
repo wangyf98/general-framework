1950020dd6fe92d28b8b7eb3d9e16067d2e1124e
wangyongfeng <wang.yongfneg@ani.link>
2021-09-15 22:36:11 +0800

    git commit 'add class and interface'

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,20 @@
+package controller;
+
+/**
+ * 用户相关Controller
+ */
+public class UserController {
+    /**
+     * 注册
+     */
+    public void register(){
+
+    }
+
+    /**
+     * 登录
+     */
+    public void login(){
+
+    }
+}
